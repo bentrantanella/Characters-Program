4079d15166f1173f7049d4630f7f3b060946c866
20btrantanella <20btrantanella@WA-248-9579.schools.westford-ma.gov>
2019-11-12 08:47:01 -0500

    New code

---
+++ Characters Program/src/CharactersGUI.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+import BreezySwing.*;
+
+public class CharactersGUI extends GBFrame {
+	
+	public static void main(String[] args) {
+		JFrame frm = new CharactersGUI();
+		frm.setTitle("Longest Non-Decreasing Sequence");
+		frm.setSize(300, 300);
+		frm.setVisible(true);
+	}
+
+	
+}
